e69f2a5c136663fadb00896cdd78f4ee124b7e42
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-01 19:01:22 +0100

    tests employee setters

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -33,4 +33,24 @@ public class ManagerTest {
     }
 
 
+    @Test
+    public void canSetName(){
+        manager.setName("Dom");
+        assertEquals("Dom", manager.getName());
+    }
+
+    @Test
+    public void canSetNiNumber(){
+        manager.setNiNumber("1");
+        assertEquals("1", manager.getNiNumber());
+    }
+
+    @Test
+    public void canSetSalary(){
+        manager.setSalary(20000);
+        assertEquals(20000, manager.getSalary(), 0.01);
+    }
+
+
+
 }
